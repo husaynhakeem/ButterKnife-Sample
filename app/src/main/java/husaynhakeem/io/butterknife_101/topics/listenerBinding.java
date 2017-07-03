c1b27e77fd83ee95b7198faaf8bb3e9c0b1839ff
Husayn Hakeem <husaynhakeem@gmail.com>
2017-07-03 14:04:30 +0100

    Listener binding - Done

--- app/src/main/java/husaynhakeem/io/butterknife_101/topics/listenerBinding.java
+++ app/src/main/java/husaynhakeem/io/butterknife_101/topics/listenerBinding.java
@@ -1,15 +1,35 @@
 package husaynhakeem.io.butterknife_101.topics;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import husaynhakeem.io.butterknife_101.R;
 
 public class listenerBinding extends AppCompatActivity {
+
+
+    private Toast toast;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listener_binding);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick({R.id.btn_1, R.id.btn_2, R.id.btn_3, R.id.btn_4})
+    public void handleButtonsClick(Button button) {
+
+        if (toast != null)
+            toast.cancel();
+
+        toast = Toast.makeText(this, "Button " + button.getText() + " clicked", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
