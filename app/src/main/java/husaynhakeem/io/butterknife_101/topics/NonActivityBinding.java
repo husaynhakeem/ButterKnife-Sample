4f2c3b2d8b68cc81f057d7f67594090a5faf82a4
Husayn Hakeem <husaynhakeem@gmail.com>
2017-07-03 13:23:34 +0100

    Non activity binding - Done

--- app/src/main/java/husaynhakeem/io/butterknife_101/topics/NonActivityBinding.java
+++ app/src/main/java/husaynhakeem/io/butterknife_101/topics/NonActivityBinding.java
@@ -1,8 +1,16 @@
 package husaynhakeem.io.butterknife_101.topics;
 
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import husaynhakeem.io.butterknife_101.R;
 
 public class NonActivityBinding extends AppCompatActivity {
@@ -10,6 +18,24 @@ public class NonActivityBinding extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_non_binding);
+        setContentView(R.layout.activity_non_activity_binding);
+    }
+
+
+    public static class NonActivityBindingFragment extends Fragment {
+
+
+        @BindView(R.id.textView)
+        TextView textView;
+
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_non_activity_binding, container, false);
+            ButterKnife.bind(this, rootView);
+            textView.setText(getString(R.string.text_non_activity_binding));
+            return rootView;
+        }
     }
 }
